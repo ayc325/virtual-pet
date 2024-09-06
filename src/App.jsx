f0f816cc3a3bc146bc48d6dba3cf260f5b94c631
Angela Cui <99048369+ayc325@users.noreply.github.com>
2024-09-06 16:37:15 -0400

    trying things

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1> Selector</h1>
+      <h2>Count: </h2>
+      <img className = "srite" src = "./assets/sprite-example.webp"></img>
+    </div>
+  )
+}
+
+export default App
